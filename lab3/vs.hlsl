c6ab6e54f944373a7b4c48f163bf7f459d204ba2
Vladimir Skvortsov <hi@vladimirskvortsov.com>
2025-02-19 01:18:11 +0300

    feat: enhance shaders readability

--- lab3/vs.hlsl
+++ lab3/vs.hlsl
@@ -1,9 +1,9 @@
 cbuffer ModelBuffer : register(b0) {
-    matrix m;
+    float4x4 model;
 };
 
 cbuffer VPBuffer : register(b1) {
-    matrix vp;
+    float4x4 vp;
 };
 
 struct VS_INPUT {
@@ -18,8 +18,8 @@ struct PS_INPUT {
 
 PS_INPUT main(VS_INPUT input) {
     PS_INPUT output;
-    float4 pos = float4(input.position, 1.0);
-    output.position = mul(pos, m);
+    float4 worldPos = float4(input.position, 1.0);
+    output.position = mul(worldPos, model);
     output.position = mul(output.position, vp);
     output.color = input.color;
     return output;
